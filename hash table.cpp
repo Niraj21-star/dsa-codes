f0c82fcc934baa5e3d1642371dc99909442ae241
Niraj Sanjay Karnawat <karnawatniraj@gmail.com>
2025-11-11 21:18:25 +0530

    Enhance hash table with multiple insertion methods
    Refactored hash table implementation to include linear, quadratic, and double hashing techniques for insertion. Updated main menu for user interaction and display functionality.

--- hash table.cpp
+++ hash table.cpp
@@ -1,43 +1,112 @@
 #include <stdio.h>
 #define SIZE 10
 
-int linear_probing(int a[], int key){
-    int index = key % SIZE;
-    int i = 0;
-    while (a[(index + i) % SIZE] != -1 && i < SIZE)
-        i++;
-    if (i == SIZE) return -1; 
-    return (index + i) % SIZE;
+long directory[SIZE];
+
+void init() {
+    for (int i = 0; i < SIZE; i++)
+        directory[i] = -1;
 }
 
-int main(){
-    int a[SIZE], ch, key, index;
-    for(int i=0; i<SIZE; i++) a[i] = -1;
+void linear_insert(long number) {
+    int key = number % SIZE;
+    int i = 0;
 
-    do{
-        printf("\n1. Insert\n2. Display\n3. Exit\nEnter choice: ");
+    while (directory[(key + i) % SIZE] != -1 && i < SIZE)
+        i++;
+
+    if (i == SIZE)
+        printf("Hash table is full! Cannot insert %ld\n", number);
+    else {
+        int index = (key + i) % SIZE;
+        directory[index] = number;
+        printf("Inserted %ld at index %d (Linear Probing)\n", number, index);
+    }
+}
+
+void quadratic_insert(long number) {
+    int key = number % SIZE;
+    int i = 0;
+
+    while (directory[(key + (i * i)) % SIZE] != -1 && i < SIZE)
+        i++;
+
+    if (i == SIZE)
+        printf("Hash table is full! Cannot insert %ld\n", number);
+    else {
+        int index = (key + (i * i)) % SIZE;
+        directory[index] = number;
+        printf("Inserted %ld at index %d (Quadratic Probing)\n", number, index);
+    }
+}
+
+void double_insert(long number) {
+    int h1 = number % SIZE;
+    int h2 = 7 - (number % 7); 
+    int i = 0;
+
+    while (directory[(h1 + i * h2) % SIZE] != -1 && i < SIZE)
+        i++;
+
+    if (i == SIZE)
+        printf("Hash table is full! Cannot insert %ld\n", number);
+    else {
+        int index = (h1 + i * h2) % SIZE;
+        directory[index] = number;
+        printf("Inserted %ld at index %d (Double Hashing)\n", number, index);
+    }
+}
+void display() {
+    printf("\n------ Telephone Directory ------\n");
+    printf("Index\tNumber\n");
+    for (int i = 0; i < SIZE; i++)
+        printf("%d\t%ld\n", i, directory[i]);
+}
+
+int main() {
+    init();
+    int ch, method;
+    long number;
+
+    do {
+        printf("\n--- MAIN MENU ---\n");
+        printf("1. Insert a record\n");
+        printf("2. Display hash table\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
         scanf("%d", &ch);
 
-        if(ch == 1){
-            printf("Enter telephone number (key): ");
-            scanf("%d", &key);
-            index = linear_probing(a, key);
-            if(index == -1)
-                printf("Hash table full!\n");
-            else {
-                a[index] = key;
-                printf("Inserted %d at index %d\n", key, index);
-            }
+        switch (ch) {
+            case 1:
+                printf("Enter telephone number: ");
+                scanf("%ld", &number);
+                printf("\nChoose hashing technique:\n");
+                printf("1. Linear Probing\n2. Quadratic Probing\n3. Double Hashing\n");
+                scanf("%d", &method);
+                switch (method) {
+                    case 1:
+                        linear_insert(number);
+                        break;
+                    case 2:
+                        quadratic_insert(number);
+                        break;
+                    case 3:
+                        double_insert(number);
+                        break;
+                    default:
+                        printf("Invalid technique choice!\n");
+                }
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                printf("Exiting program...\n");
+                break;
+            default:
+                printf("Invalid main menu choice!\n");
         }
-        else if(ch == 2){
-            printf("\nHash Table:\n");
-            for(int i=0; i<SIZE; i++)
-                printf("%d -> %d\n", i, a[i]);
-        }
-        else if(ch != 3)
-            printf("Invalid choice!\n");
-    }while(ch != 3);
+    } while (ch != 3);
 
     return 0;
 }
-
